f4831185c9d8a84ca6c1b0551d840962848d969e
joao <joao.dev94@gmail.com>
2023-01-20 20:39:40 -0300

    Requisito 02

--- src/routes/talker.router.js
+++ src/routes/talker.router.js
@@ -1,11 +1,21 @@
 const express = require('express');
-const { getTalkers } = require('../utils/talkerService');
+const { getTalkers } = require('../utils/talkerUtils');
 
 const router = express.Router();
 
 router.get('/talker', async (_req, res) => {
     const talkers = await getTalkers();
     res.status(200).json(talkers);
+});
+
+router.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const allTalkers = await getTalkers();
+  const talkerFiltered = allTalkers.filter((talker) => talker.id === Number(id));
+  if (talkerFiltered.length) {
+    return res.status(200).json(...talkerFiltered);
+  }
+  return res.status(404).send({ message: 'Pessoa palestrante não encontrada' });
 });
 
 module.exports = router;
