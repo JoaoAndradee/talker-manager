666e83e6d2483517ba672d69e3e59f234843241d
joao <joao.dev94@gmail.com>
2023-01-23 01:49:38 -0300

    Requisito 04

--- src/routes/login.router.js
+++ src/routes/login.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const generateToken = require('../utils/cryptoUtils');
+const loginMiddleware = require('../utils/loginUtils');
 
 const router = express.Router();
 
-router.post('/login', (req, res) => {
+router.post('/login', loginMiddleware, (_req, res) => {
   const token = generateToken();
   res.status(200).json({ token });
 });
